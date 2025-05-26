6c4ac2da8e8f122040db6eaecbad9c48f9fb3a46
ian <ian@ejemplo.com>
2025-05-26 22:53:25 +0200

    Segundo

---
+++ js/datosColeccion.js
@@ -0,0 +1,16 @@
+const enfrentamientos = [
+    {fecha: 2302, direccion: "2222", equipoRojo: "sdds", EquipoAzul: "Ssss", campeonatoNombre: "222", campeonatoAnyo: 2000}
+]
+const equipos = [
+    {uuid: '123e4567-e89b-12d3-a456-426614174000'},
+    {uuid: '223e4567-e89b-12d3-a456-426614174001'},
+    {uuid: '323e4567-e89b-12d3-a456-426614174002'}
+]
+/*
+Haz esto:
+const enfrentamientos = [
+    {fecha: , direccion: "", equipoRojo: "", EquipoAzul: "", campeonatoNombre: "", campeonatoAnyo: }
+]
+
+DE LOS DATOS DE:
+*/
