767e0f6b5ef153b1d4272068fa58b6301febbbec
ian <ian@ejemplo.com>
2025-05-28 11:36:49 +0200

    Cambio Tabla

--- js/datosColeccion.js
+++ js/datosColeccion.js
@@ -1,13 +1,18 @@
 const enfrentamientos = [
-     { fecha: "2021-10-28", direccion: "Hovet Arena", equipoRojo: "MadKoi", equipoAzul: "G2 Esports", campeonatoNombre: "PGL Major Stockholm", campeonatoAnyo: 2021 },
-  { fecha: "2021-10-30", direccion: "Hovet Arena", equipoRojo: "G2 Esports", equipoAzul: "NAVI", campeonatoNombre: "PGL Major Stockholm", campeonatoAnyo: 2021 },
-  { fecha: "2021-11-02", direccion: "Hovet Arena", equipoRojo: "NAVI", equipoAzul: "MadKoi", campeonatoNombre: "PGL Major Stockholm", campeonatoAnyo: 2021 },
-  { fecha: "2023-03-15", direccion: "Eden Arena", equipoRojo: "G2 Esports", equipoAzul: "MadKoi", campeonatoNombre: "ESL Pro League Season 17", campeonatoAnyo: 2023 },
-  { fecha: "2023-03-17", direccion: "Eden Arena", equipoRojo: "MadKoi", equipoAzul: "NAVI", campeonatoNombre: "ESL Pro League Season 17", campeonatoAnyo: 2023 },
+    { fecha: "2025-05-29", direccion: "Online", equipoRojo: "G2 Esports", equipoAzul: "Mad Lions", campeonatoNombre: "YGames PRO Series", campeonatoAnyo: 2025 },
+  { fecha: "2025-05-28", direccion: "Online", equipoRojo: "Mad Lions", equipoAzul: "NAVI", campeonatoNombre: "YGames PRO Series", campeonatoAnyo: 2025 },
+  { fecha: "2025-05-28", direccion: "Online", equipoRojo: "NAVI", equipoAzul: "G2 Esports", campeonatoNombre: "YGames PRO Series", campeonatoAnyo: 2025 },
+    { fecha: "2023-03-15", direccion: "Eden Arena", equipoRojo: "G2 Esports", equipoAzul: "Mad Lions", campeonatoNombre: "ESL Pro League Season 17", campeonatoAnyo: 2023 },
+  { fecha: "2023-03-17", direccion: "Eden Arena", equipoRojo: "Mad Lions", equipoAzul: "NAVI", campeonatoNombre: "ESL Pro League Season 17", campeonatoAnyo: 2023 },
   { fecha: "2023-03-19", direccion: "Eden Arena", equipoRojo: "NAVI", equipoAzul: "G2 Esports", campeonatoNombre: "ESL Pro League Season 17", campeonatoAnyo: 2023 },
-  { fecha: "2022-12-14", direccion: "Etihad Arena", equipoRojo: "NAVI", equipoAzul: "MadKoi", campeonatoNombre: "BLAST Premier World Final", campeonatoAnyo: 2022 },
-  { fecha: "2022-12-16", direccion: "Etihad Arena", equipoRojo: "MadKoi", equipoAzul: "G2 Esports", campeonatoNombre: "BLAST Premier World Final", campeonatoAnyo: 2022 },
-  { fecha: "2022-12-18", direccion: "Etihad Arena", equipoRojo: "G2 Esports", equipoAzul: "NAVI", campeonatoNombre: "BLAST Premier World Final", campeonatoAnyo: 2022 }
+   { fecha: "2022-12-14", direccion: "Etihad Arena", equipoRojo: "NAVI", equipoAzul: "Mad Lions", campeonatoNombre: "BLAST Premier World Final", campeonatoAnyo: 2022 },
+  { fecha: "2022-12-16", direccion: "Etihad Arena", equipoRojo: "Mad Lions", equipoAzul: "G2 Esports", campeonatoNombre: "BLAST Premier World Final", campeonatoAnyo: 2022 },
+  { fecha: "2022-12-18", direccion: "Etihad Arena", equipoRojo: "G2 Esports", equipoAzul: "NAVI", campeonatoNombre: "BLAST Premier World Final", campeonatoAnyo: 2022 }, 
+  { fecha: "2021-10-28", direccion: "Hovet Arena", equipoRojo: "Mad Lions", equipoAzul: "G2 Esports", campeonatoNombre: "PGL Major Stockholm", campeonatoAnyo: 2021 },
+  { fecha: "2021-10-30", direccion: "Hovet Arena", equipoRojo: "G2 Esports", equipoAzul: "NAVI", campeonatoNombre: "PGL Major Stockholm", campeonatoAnyo: 2021 },
+  { fecha: "2021-11-02", direccion: "Hovet Arena", equipoRojo: "NAVI", equipoAzul: "Mad Lions", campeonatoNombre: "PGL Major Stockholm", campeonatoAnyo: 2021 }
+
+ 
 ]
 const equipos = [
     {uuid: '123e4567-e89b-12d3-a456-426614174000'},
